3664f3f5f0bba34aadf1eaf21e38114588c58797
Diogo Moreira <diogom.o.moreira@gmail.com>
2020-01-23 20:15:44 +0000

    SideNav added

---
+++ src/components/header.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+export default class Header extends Component {
+  render() {
+    /* Example with router: https://www.npmjs.com/package/@trendmicro/react-sidenav */
+    return (
+      <SideNav>
+          <SideNav.Toggle />
+          <SideNav.Nav defaultSelected="home">
+              <NavItem eventKey="home">
+                  <NavIcon>
+                      <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                  </NavIcon>
+                  <NavText>
+                      Home
+                  </NavText>
+              </NavItem>
+              <NavItem eventKey="charts">
+                  <NavIcon>
+                      <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                  </NavIcon>
+                  <NavText>
+                      Charts
+                  </NavText>
+                  <NavItem eventKey="charts/linechart">
+                      <NavText>
+                          Line Chart
+                      </NavText>
+                  </NavItem>
+                  <NavItem eventKey="charts/barchart">
+                      <NavText>
+                          Bar Chart
+                      </NavText>
+                  </NavItem>
+              </NavItem>
+          </SideNav.Nav>
+      </SideNav>
+    )
+  }
+}
