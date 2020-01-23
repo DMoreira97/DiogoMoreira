b6ac89f37abce7f98fc6d58312944c9bae18bf8d
DMoreira97 <diogom.o.moreira@gmail.com>
2020-01-23 19:23:11 +0000

    Updates

---
+++ precache-manifest.a353816ddf2c2386ee21e9e2825d4205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d80a3dba1c431a194179149573c9554",
+    "url": "/index.html"
+  },
+  {
+    "revision": "081244b36f15077ed34f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2750509e30c4c0dd89e",
+    "url": "/static/js/2.16ace03a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.16ace03a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "081244b36f15077ed34f",
+    "url": "/static/js/main.5834a50b.chunk.js"
+  },
+  {
+    "revision": "571a66be3569138ded94",
+    "url": "/static/js/runtime-main.b23594ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
